cb6bdb9097082a4bbf81202640d058384032de71
Bruno <bruno178pm@gmail.com>
2019-06-27 03:50:52 -0300

    Login hecho

--- app/src/main/java/com/example/pbt/view/SplashActivity.java
+++ app/src/main/java/com/example/pbt/view/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
             new Runnable() {
                 @Override
                 public void run() {
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    startActivity(new Intent(SplashActivity.this, Login.class));
                     finish();
                 }
             }, SPLASH_DURATION
